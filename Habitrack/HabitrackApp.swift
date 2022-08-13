877a0de301a5745d3c9311569fef8b594b078351
mcantop <maciejcantop@icloud.com>
2022-08-13 20:42:59 +0200

    Initial Commit

---
+++ Habitrack/HabitrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitrackApp.swift
+//  Habitrack
+//
+//  Created by Maciej on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
